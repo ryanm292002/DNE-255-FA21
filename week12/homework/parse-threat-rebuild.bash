453b5dbb4a3e5b350d7a56a0de9563438f344651
ryanm292002 <ryan.morrissey@champlain.edu>
2021-11-15 16:09:32 -0500

    mothra

--- week12/homework/parse-threat-rebuild.bash
+++ week12/homework/parse-threat-rebuild.bash
@@ -1,11 +1,14 @@
 
 
 #!/bin/bash
-#Storyline : Extract IPs from https://www.projecthoneypot.org/list_of_ips.php and create a fiewall ruleset
+#Storyline : Extract IPs from  https://www.projecthoneypot.org/list_of_ips.php?t=d, https://www.projecthoneypot.org/list_of_ips.php?t=s, https://www.projecthoneypot.org/list_of_ips.php?t=p and create a fiewall ruleset
 
 #134.119.216.167 | CR	Bad Event	214,874	2017-03-18 	2021-11-14
 
-#wget https://www.projecthoneypot.org/list_of_ips.php -O /tmp/access.log
+wget https://www.projecthoneypot.org/list_of_ips.php?t=d -O /tmp/access.log
+wget https://www.projecthoneypot.org/list_of_ips.php?t=s -O /tmp/access.log
+wget https://www.projecthoneypot.org/list_of_ips.php?t=p -O /tmp/access.log
+
 
 egrep -o '[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}' /tmp/access.log | sort -u | tee badIPs.txt
 
