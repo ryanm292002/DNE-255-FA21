afe656240a03f9fd180c60a28f7660e08d3244aa
ryanm292002 <ryan.morrissey@champlain.edu>
2021-11-14 21:31:06 -0500

    rebuild

---
+++ week12/homework/parse-threat-rebuild.bash
@@ -0,0 +1,19 @@
+
+
+#!/bin/bash
+#Storyline : Extract IPs from https://www.projecthoneypot.org/list_of_ips.php and create a fiewall ruleset
+
+#134.119.216.167 | CR	Bad Event	214,874	2017-03-18 	2021-11-14
+
+#wget https://www.projecthoneypot.org/list_of_ips.php -O /tmp/access.log
+
+egrep -o '[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}' /tmp/access.log | sort -u | tee badIPs.txt
+
+
+for eachIP in $(cat badIPs.txt)
+do
+	#echo "block in from ${eachIP} to any" | tee -a pf.conf
+
+        echo "iptables -A INPUT -s ${eachIP} -j DROP" | tee -a badIPS.iptables
+done
+
